7886544de89742d72285a489f71d68149ddcca1e
Malena Schoeni <mschoeni789@gmail.com>
2019-10-04 09:35:09 -0500

    added receipt table

--- customerBackend.php
+++ customerBackend.php
@@ -6,15 +6,18 @@ $vanilla = $_POST["vanilla"];
 $peanut = $_POST["peanut"];
 $mint = $_POST["mint"];
 $shipping = $_POST["shipping"];
+$shipCost = 0;
 
 $subtotal = ($choco * 15) + ($vanilla * 20) + ($peanut * 30) + ($mint * 32);
 if ($shipping == "overnight")
 {
     $subtotal = $subtotal + 50;
+    $shipCost = 50;
 }
 else if ($shipping == "three")
 {
     $subtotal = $subtotal + 5;
+    $shipCost = 5;
 }
 
 echo "<br>Thank you for your purchase. Here is a copy of your receipt";
@@ -31,7 +34,9 @@ echo "</tr><tr><th>Peanut</th>";
 echo "<td>" . $peanut . "</td><td>30</td>" . "<td>" . $peanut*30 . "</td>";
 echo "</tr><tr><th>Mint</th>";
 echo "<td>" . $mint . "</td><td>32</td>" . "<td>" . $mint*32 . "</td>";
-echo "</tr></table>";
+echo "</tr><tr><th>Shipping</th><td colspan = 2>" . $shipping . "</td><td>" . $shipCost . "</td></tr>";
+echo "<tr><th colspan=3>Total cost</th> <td>" . $subtotal . "</tr>";
+echo "</table>";
 
 
 
